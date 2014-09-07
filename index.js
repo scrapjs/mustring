cae23da50733fe331dd260e4f1ffe8c3735b79cb
Dmitry <df.creative@gmail.com>
2014-09-08 02:40:28 +0400

    Expand scope

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-var S = module.exports = {
-	//camel-case → CamelCase
-	camel: function (str){
-		return str && str.replace(/-[a-z]/g, function(match, position){
-			return S.upper(match[1])
-		})
-	},
-
-	//CamelCase → camel-case
-	dashed: function (str){
-		return str && str.replace(/[A-Z]/g, function(match, position){
-			return (position ? '-' : '') + S.lower(match)
-		})
-	},
-
-	//uppercaser
-	upper: function (str){
-		return str.toUpperCase();
-	},
-
-	//lowercasify
-	lower: function (str){
-		return str.toLowerCase();
-	},
-
-	//aaa → Aaa
-	capfirst: function (str){
-		str+='';
-		if (!str) return str;
-		return S.upper(str[0]) + str.slice(1);
-	},
-
-	// onEvt → envt
-	unprefixize: function (str, pf){
-		return (str.slice(0,pf.length) === pf) ? S.lower(str.slice(pf.length)) : str;
-	}
+module.exports = {
+	camel:camel,
+	dashed:dashed,
+	upper:upper,
+	lower:lower,
+	capfirst:capfirst,
+	unprefixize:unprefixize
 };
+
+//camel-case → CamelCase
+function camel(str){
+	return str && str.replace(/-[a-z]/g, function(match, position){
+		return upper(match[1])
+	})
+}
+
+//CamelCase → camel-case
+function dashed(str){
+	return str && str.replace(/[A-Z]/g, function(match, position){
+		return (position ? '-' : '') + lower(match)
+	})
+}
+
+//uppercaser
+function upper(str){
+	return str.toUpperCase();
+}
+
+//lowercasify
+function lower(str){
+	return str.toLowerCase();
+}
+
+//aaa → Aaa
+function capfirst(str){
+	str+='';
+	if (!str) return str;
+	return upper(str[0]) + str.slice(1);
+}
+
+// onEvt → envt
+function unprefixize(str, pf){
+	return (str.slice(0,pf.length) === pf) ? lower(str.slice(pf.length)) : str;
+}
